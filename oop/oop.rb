d22ac23949d7f7eb44ed50434426a62f89dca3ec
Aerdayne <daenskus@gmail.com>
2019-09-06 23:58:50 +0300

    Adjustments

--- oop/oop.rb
+++ oop/oop.rb
@@ -1,119 +1,120 @@
+# frozen_string_literal: true
+
+# :nodoc:
 class Station
-    attr_reader :name
+  attr_reader :trains
 
-    def initialize(name)
-        @name = name
-        @trains = []
-    end
+  def initialize(name)
+    @name = name
+    @trains = []
+  end
 
-    def host(train)
-        @trains << train if train.current == self
-    end
-    
-    def depart(train)
-        @trains.delete(train) if train.current == self
-    end
+  def host(train)
+    @trains << train if train.current == self
+  end
 
-    def list
-        @trains.each { |train| print(train) }
-    end
+  def depart(train)
+    @trains.delete(train) if train.current == self
+  end
 
-    def list_passenger
-        list = @trains.select { |train| train.type == 'passenger' }
-        list.each { |train| print(train) }
-    end
-
-    def list_freight
-        list = @trains.select { |train| train.type == 'freight' }
-        list.each { |train| print(train) }
-    end
+  def list_specififc(type)
+    @trains.select { |train| train.type == type }
+  end
 end
 
+# :nodoc:
 class Route
-    attr_reader :departure, :destination, :intermediate
+  attr_reader :course
 
-    def initialize(departure,destination)
-        @departure = departure
-        @destination = destination
-        @intermediate = []
-    end
+  def initialize(departure, destination)
+    @course = [departure, destination]
+  end
 
-    def add_intermediate(station)
-        @intermediate << station
-    end
+  def departure
+    @course[0]
+  end
 
-    def remove_intermediate(station)
-        @intermediate.delete(station)
-    end
-    
-    def list_route
-        print(@departure)
-        @intermediate.each { |station| print(station) }
-        print(@destination)
-    end
+  def destination
+    @course[-1]
+  end
+
+  def intermediate
+    @course[1..-2]
+  end
+
+  def add_intermediate(station)
+    @course.insert(-2, station)
+  end
+
+  def remove_intermediate(station)
+    @course.delete(station) if @course[1..-2].delete(station)
+  end
+
+  def list_route
+    @course.each { |station| print(station) }
+  end
 end
 
+# :nodoc:
 class Train
-    attr_reader :speed
-    attr_reader :car_quantity
-    attr_reader :type
-    attr_reader :current
+  attr_reader :speed, :car_quantity, :type, :route
 
-    def initialize(id,type,car_quantity)
-        @id = id
-        @type = type
-        @car_quantity = car_quantity
-        @speed = 0
-    end
+  def initialize(id, type, car_quantity)
+    @id = id
+    @type = type
+    @car_quantity = car_quantity
+    @speed = 0
+  end
 
-    def speedup
-        @speed += 10
-    end
+  def increase_speed(value)
+    @speed += value
+  end
 
-    def slowdown
-        @speed -= 10
-    end
+  def decrease_speed(value)
+    @speed -= value unless @speed < value
+  end
 
-    def couple
-        @speed == 0 ? @car_quantity += 1 : puts("Train is in motion")
-    end
+  def couple
+    @car_quantity += 1 unless @speed.zero?
+  end
 
-    def decouple
-        @speed == 0 ? @car_quantity -= 1 : puts("Train is in motion")
-    end
+  def decouple
+    @car_quantity -= 1 unless @speed.zero?
+  end
 
-    def set_route(route)
-        @route = route
-        @route_whole = [@route.departure, @route.intermediate, @route.destination].flatten
-        @current = @route_whole[0]
-    end
+  def assign_route(route)
+    @route = route
+    @current_station_id = 0
+    @route.course[@current_station_id].host(self)
+  end
 
-    def forwards
-        index = @route_whole.index(@current)
-        if index.nil? || @route_whole[index + 1] == nil
-            puts('Train has reached destination') 
-        else
-            @current = @route_whole[index + 1]
-        end
-    end
+  # move to the next station
+  def forwards
+    return if @current_station_id == @route.course.length - 1
 
-    def backwards
-        index = @route_whole.index(@current)
-        if index.nil? || @route_whole[index - 1] == nil || index == 0
-            puts('Train has reached destination')
-        else 
-            @current = @route_whole[index - 1]
-        end
-    end
+    @route.course[@current_station_id].depart(self)
+    @current_station_id += 1
+    @route.course[@current_station_id].host(self)
+  end
 
-    def list
-        index = @route_whole.index(@current)
-        print('Current - ' + @route_whole[index].name)
-        if index != 0
-            print('Previous - ' + @route_whole[index - 1].name)
-        end
-        if @route_whole[index + 1] != nil
-            print('Next - ' + @route_whole[index + 1].name)
-        end
-    end
+  # move to the previous station
+  def backwards
+    return if @current_station_id.zero?
+
+    @route.course[@current_station_id].depart(self)
+    @current_station_id -= 1
+    @route.course[@current_station_id].host(self)
+  end
+
+  def current
+    @route.course[@current_station_id]
+  end
+
+  def previous
+    @route.course[@current_station_id - 1] unless @current_station_id.zero?
+  end
+
+  def next
+    @route.course[@current_station_id + 1] unless @current_station_id == @route.route.length
+  end
 end
